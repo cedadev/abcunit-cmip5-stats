699751547e4bc70cf4014139abbe0dc8100671d9
Eleanor Smith <esmith88@jasmin-sci1.ceda.ac.uk>
2019-12-02 16:35:44 +0000

    added tests for multiple netCDF files

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,7 +1,8 @@
 import xarray as xr
 import numpy as np
 import pandas as pd
-
+import argparse
+import glob
 
 def test_xarray_open_bad_path_fail():
     fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_-190911.nc'
@@ -61,7 +62,6 @@ def test_xarray_open_time_dimension_fail_dims_do_not_exist():
 	fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
     	ds = xr.open_dataset(fpath)
     	time = ds.sel(dims='time')
-    	print(time)
     except ValueError as exc:
 	pass
 
@@ -71,7 +71,6 @@ def test_xarray_open_time_dimension_fail_key_error():
 	fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
     	ds = xr.open_dataset(fpath)
     	time = ds.sel(lat=360, lon=90)
-    	print(time)
     except KeyError as exc:
 	pass
 
@@ -83,10 +82,75 @@ def test_xarray_open_time_dimension():
         time = ds.sel(lat=0, lon=0)
         
 
-
 def test_slice_a_time_range():
 	fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
         ds = xr.open_dataset(fpath)
 	time_slice = ds.time.loc['1900-01-01':'2000-01-01']
-	print(time_slice)
+
+
+def test_find_temporal_max_incorrect_shape():
+        try:
+		fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
+        	ds = xr.open_dataset(fpath)
+        	print('shape =', ds.shape)
+        	maximum = ds.max(dim='time')
+        	print('max =', maximum)
+        	print('max shape =', maximum.shape)
+	
+	except AttributeError as exc:
+		pass
+
+def test_find_temporal_max():
+        fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
+        ds = xr.open_dataset(fpath)
+        print('shape =', ds.rh.shape)
+	maximum = ds.rh.max(dim='time')
+        print('max =', maximum)
+	print('max shape =', maximum.shape)
+
+
+def test_find_temporal_min():
+        fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
+        ds = xr.open_dataset(fpath)
+        minimum = ds.rh.min(dim='time')
+        print('min =', minimum)
+        print('min shape =', minimum.shape)
+
+
+def test_find_temporal_mean():
+        fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/rh_Lmon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
+        ds = xr.open_dataset(fpath)
+        mean = ds.rh.mean(dim='time')
+        print('mean =', mean)
+        print('mean shape =', mean.shape)		
+
+"""def create_parser(args):
+        parser = argparse.ArgumentParser(description='prints given arguments')
+        parser.add_argument('-a', help='first argument to print', dest='a')
+
+        args = parser.parse_args(args)
+
+        if args.a:
+                print(args['a'])
+
+
+def test_arg_parser():
+        parser = create_parser(['hi'])
+        assertEqual(parser, 'hi')"""
+
+def test_open_multiple_files():
+	pattern = '/badc/cmip5/data/cmip5/output1/{model}/historical/mon/land/Lmon/{ensemble}/latest/{var_id}/*.nc'
+	model = 'MOHC/HadGEM2-ES'
+	ensemble = 'r1i1p1'
+	var_id='rh'
+	glob_pattern = pattern.format(model=model, ensemble=ensemble, var_id=var_id)
+    	nc_files = glob.glob(glob_pattern)
+	return xr.open_mfdataset(nc_files)
+
+
+def test_max_multiple_files():
+	ds = test_open_multiple_files()
+	maximum = ds.rh.max(dim='time')
+	print('max_multiple =', maximum)
+	print('max_multiple shape =', maximum.shape)
 
