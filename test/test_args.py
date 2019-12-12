7716956e479dfaa193e51f4c49787702c6ac6524
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2019-12-12 15:52:58 +0000

    annotated tests

--- test/test_args.py
+++ test/test_args.py
@@ -6,7 +6,7 @@ from run_all import arg_parse_all
 from run_batch import arg_parse_batch
 from run_chunk import arg_parse_chunk
 
-
+# check arguemnts are parsed correctly
 def test_good_args1():
     sys.argv = 'run_all.py -s mean'.split()
     args = arg_parse_all()
@@ -41,7 +41,7 @@ def test_good_args4():
     assert args.stat == ['mean']
 
 
-#test that stat is required
+# test that stat is required
 def test_arg_parse_all_1():
     try:
         sys.argv = 'run_chunk.py'.split()
@@ -49,13 +49,13 @@ def test_arg_parse_all_1():
     except SystemExit as exc:
         pass
 
-#test works with only stat
+# test works with only stat
 def test_arg_parse_all_2():
     sys.argv = 'run_chunk.py -s min'.split()
     arg_parse_all()
 
 
-#test defaults are correct
+# test defaults are correct
 def test_arg_parse_all_3():
     sys.argv = 'run_chunk.py -s min'.split()
     args = arg_parse_all()
@@ -64,7 +64,7 @@ def test_arg_parse_all_3():
     assert args.var_id == defaults.variables
 
 
-#testing that model argument is required
+# testing that model argument is required
 def test_arg_parse_batch():
     try:
         sys.argv = 'run_chunk.py -s min'.split()
@@ -72,7 +72,7 @@ def test_arg_parse_batch():
     except SystemExit as exc:
         pass
 
-#test defaults are correct
+# test defaults are correct
 def test_arg_parse_batch_2():
     sys.argv = 'run_chunk.py -s min -m BCC/bcc-csm1-1'.split()
     args = arg_parse_all()
@@ -80,7 +80,7 @@ def test_arg_parse_batch_2():
     assert args.var_id == defaults.variables
 
 
-#testing that other arguments are required
+# testing that other arguments are required
 def test_arg_parse_chunk_1():
     try:
         sys.argv = 'run_chunk.py -s min'.split()
@@ -88,7 +88,7 @@ def test_arg_parse_chunk_1():
     except SystemExit as exc:
         pass
 
-#testing ensemble is required
+# testing ensemble is required
 def test_arg_parse_chunk_2():
     try:
         sys.argv = 'run_chunk.py -s min -m BCC/bcc-csm1-1'.split()
@@ -96,7 +96,7 @@ def test_arg_parse_chunk_2():
     except SystemExit as exc:
         pass
 
-#check variables defaults to all
+# check variables defaults to all
 def test_arg_parse_chunk_3():
     sys.argv = 'run_chunk.py -s min -m BCC/bcc-csm1-1 -e r11i1p1'.split()
     args = arg_parse_chunk()
--- test/test_run_all.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-
-#testing print statement is correct
-def test_running_print_statement():
-    pass
