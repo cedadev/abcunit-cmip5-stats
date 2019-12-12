28bac968abae9207f852623beba71e9955eb1552
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2019-12-12 15:48:17 +0000

    tidied up argparse tests

--- test/test_args.py
+++ test/test_args.py
@@ -6,41 +6,39 @@ from run_all import arg_parse_all
 from run_batch import arg_parse_batch
 from run_chunk import arg_parse_chunk
 
-# from arg_parsers import parse_args_stat, parse_args_two
-#
-#
-# def test_good_args1():
-#     sys.argv = 'arg_parsers.py -s mean'.split()
-#     args = parse_args_stat()
-#     assert args.stat == 'mean'
-#     assert args.stat != 'min'
-#     assert args.stat != 'max'
-#
-#
-# def test_good_args2():
-#     try:
-#         sys.argv = 'arg_parsers.py -s mean -m BCC/bcc-csm1-1'.split()
-#         args = parse_args_two()
-#         assert args.model == 'BCC/bcc-csm1-1'
-#     except AssertionError as exc:
-#         pass
-#
-#
-# def test_good_args3():
-#     try:
-#         sys.argv = 'arg_parsers.py -s mean -m BCC/bcc-csm1-1'.split()
-#         args = parse_args_two()
-#         assert args.model == ['BCC/bcc-csm1-1']
-#         assert args.stat == 'mean'
-#     except AssertionError as exc:
-#         pass
-#
-#
-# def test_good_args4():
-#     sys.argv = 'arg_parsers.py -s mean -m BCC/bcc-csm1-1'.split()
-#     args = parse_args_two()
-#     assert args.model == ['BCC/bcc-csm1-1']
-#     assert args.stat == ['mean']
+
+def test_good_args1():
+    sys.argv = 'run_all.py -s mean'.split()
+    args = arg_parse_all()
+    assert args.stat == ['mean']
+    assert args.stat != ['min']
+    assert args.stat != ['max']
+
+
+def test_good_args2():
+    try:
+        sys.argv = 'run_batch.py -s mean -m BCC/bcc-csm1-1'.split()
+        args = arg_parse_batch()
+        assert args.model == 'BCC/bcc-csm1-1'
+    except AssertionError as exc:
+        pass
+
+
+def test_good_args3():
+    try:
+        sys.argv = 'run_batch.py -s mean -m BCC/bcc-csm1-1'.split()
+        args = arg_parse_batch()
+        assert args.model == ['BCC/bcc-csm1-1']
+        assert args.stat == 'mean'
+    except AssertionError as exc:
+        pass
+
+
+def test_good_args4():
+    sys.argv = 'run_batch.py -s mean -m BCC/bcc-csm1-1'.split()
+    args = arg_parse_batch()
+    assert args.model == ['BCC/bcc-csm1-1']
+    assert args.stat == ['mean']
 
 
 #test that stat is required
@@ -116,5 +114,5 @@ def test_arg_parse_return():
 
     mn = ds['rh'].min(dim='time')
     assert(mn.shape == (145, 192))
-    
+
 
